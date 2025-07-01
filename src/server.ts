621d1c4636013f127dd68672c9ea9dd3aced3fac
Victor Cantanhede <victorcantanhedblk@gmail.com>
2025-07-01 15:21:10 -0300

    cors added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import Fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
 import fastifyCookie from '@fastify/cookie';
 import { userRoutes } from './routes/userRoutes';
 import { taskRoutes } from './routes/taskRoutes';
 
 const app = Fastify({ logger: true });
 const PORT = process.env.PORT || 5000;
+
+app.register(fastifyCors, {
+    origin: process.env.FRONT_URL as string || 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+});
 
 app.register(fastifyCookie);
 app.register(userRoutes, { prefix: '/users' });
